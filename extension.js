2a261f1a566ae945c24f281a42dc77af36f35706
sergezoghbi <serge2zoghbi1@gmail.com>
2023-02-07 14:47:57 +0200

    first commit

---
+++ extension.js
@@ -0,0 +1,125 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+'use strict';
+
+const {St, Gio, Clutter, Soup, GLib} = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+let panelButton;
+let panelButtonText;
+let session;
+let sourceId = null;
+
+// Start application
+function init() {
+    log(`initializing ${Me.metadata.name}`);
+}
+
+// Add the button to the panel
+function enable() {
+    panelButton = new St.Bin({
+        style_class: "panel-button",
+    });
+
+    handle_request_api();
+    Main.panel._centerBox.insert_child_at_index(panelButton, 0);
+    sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 150, () => {
+        handle_request_api();
+        return GLib.SOURCE_CONTINUE;
+    });
+}
+
+// Remove the added button from panel
+function disable() {
+    panelButtonText = null;
+    session = null;
+    Main.panel._centerBox.remove_child(panelButton);
+
+    if (panelButton) {
+        panelButton.destroy();
+        panelButton = null;
+    }
+
+    if (sourceId) {
+        GLib.Source.remove(sourceId);
+        sourceId = null;
+    }
+}
+
+Date.prototype.yyyymmdd = function () {
+    var mm = this.getMonth() + 1;
+    var dd = this.getDate();
+    var hh = this.getHours();
+
+    return [this.getFullYear(),
+        mm,
+        dd,
+        hh
+    ].join('');
+};
+
+
+// Handle Requests API
+async function handle_request_api() {
+    let dollarQuotation = null;
+
+    try {
+        // Create a new Soup Session
+        if (!session) {
+            session = new Soup.Session({timeout: 10});
+        }
+        let time = new Date();
+        // Create body of Soup request
+        let message = Soup.Message.new_from_encoded_form(
+            "GET", "https://lirarate.org/wp-json/lirarate/v2/rates", Soup.form_encode_hash({
+                "currency": "LBP",
+                "_ver": "t" + time.yyyymmdd()
+            }));
+
+        // Send Soup request to API Server
+        await session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (_, r0) => {
+            let text = session.send_and_read_finish(r0);
+            let response = new TextDecoder().decode(text.get_data());
+            const body_response = JSON.parse(response);
+            dollarQuotation = body_response["sell"][body_response["sell"].length - 1][1];
+
+            // Sent text in Widget
+            panelButtonText = new St.Label({
+                text: " USD: LBP " + dollarQuotation,
+                y_align: Clutter.ActorAlign.CENTER,
+            });
+            panelButton.set_child(panelButtonText);
+
+            // Finish Soup Session
+            session.abort();
+        });
+    } catch (error) {
+        log(`Traceback Error in [handle_request_api]: ${error}`);
+        panelButtonText = new St.Label({
+            text: "Not working",
+            y_align: Clutter.ActorAlign.CENTER,
+        });
+        panelButton.set_child(panelButtonText);
+        session.abort();
+    }
+}
